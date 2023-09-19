36b6ea3f88671af4953bc54eeba461fd9ab492ce
Olivier <omalahel@gmail.com>
2023-09-19 11:26:38 +0200

    connection.js plus his module.exports

--- models/connections.js
+++ models/connections.js
@@ -6,3 +6,6 @@ const connectionString = process.env.CONNECTION_STRING;
 mongoose.connect(connectionString, {connectTimeoutMS: 2000})
 .then(()=> console.log('Database connected'))
 .catch(error => console.error(error))
+
+
+module.exports = connectionString;
